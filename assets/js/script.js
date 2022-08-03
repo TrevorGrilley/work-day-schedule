40d0cc0d1e82d0fe09775c70eee57d1e7af61aa7
Trevor <trevorgrilley@yahoo.com>
2022-08-02 20:02:00 -0400

    create function to save text and time to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,14 @@ function updateDateTime() {
 	dateTime.innerText = now
 };
 setInterval(updateDateTime, 1000)
+
+//Create a function to save text and time to local storage
+
+$(document).ready(function () {
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, text);
+    })  
+})
